9b7f4856d0988d9f7dd25bf3e6cae7ecc2d6b991
Nikhil Chaudhari <74759172+nikhilsc@users.noreply.github.com>
2021-05-05 14:25:37 +0530

    First commit from eclipse

---
+++ LinearLinkedList/src/com/demo/LinearLinkListDemo.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinearLinkListDemo {
+	
+	public static void main(String[] args) {
+		
+		List<String> al = new LinkedList<>();
+		al.add("A");
+		al.add("B");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
